2ec5cb8be189af64f9c950ac1ae20822a238af76
Viktor Povazhuk <viktor.povazhuk@ucu.edu.ua>
2023-06-24 11:02:05 +0300

    1. Preparation: correct size, filename, unlabeled. 2. Scrap: added exception handling.

--- data_scripts/prepare_script.py
+++ data_scripts/prepare_script.py
@@ -1,51 +1,162 @@
 import cv2
 import math
-import os
-
-DATA_FOLDER = 'data/'
+from pathlib import Path
+from tqdm import tqdm
+import pandas as pd
 
 
 def resize_img(img):
-    new_size = 300
+    new_size = 224
     h, w = img.shape[:2]
-    scale = max(h/new_size, w/new_size)
-    dim = (int(w/scale), int(h/scale))
-    horiz_board = (new_size - dim[0])/2
+    scale = max(h / new_size, w / new_size)
+    dim = (int(w / scale), int(h / scale))
+    horiz_board = (new_size - dim[0]) / 2
     horiz_board = [math.floor(horiz_board), math.ceil(horiz_board)]
-    vertic_board = (new_size - dim[1])/2
+    vertic_board = (new_size - dim[1]) / 2
     vertic_board = [math.floor(vertic_board), math.ceil(vertic_board)]
 
     res = cv2.resize(img, dim)
     res = cv2.copyMakeBorder(
-        res, vertic_board[0], vertic_board[1], horiz_board[0], horiz_board[1], cv2.BORDER_CONSTANT, value=0)
+        res,
+        vertic_board[0],
+        vertic_board[1],
+        horiz_board[0],
+        horiz_board[1],
+        cv2.BORDER_CONSTANT,
+        value=0,
+    )
 
     return res
 
 
-def augmentate_img():
-    pass
+def correct_filenames(raw_dir, conv_filenames):
+    raw_dir = Path(raw_dir)
+
+    corrected_num = 0
+    for f in raw_dir.glob("*"):
+        if f.is_dir():
+            correct_filenames(f, conv_filenames)
+            continue
+
+        # if f.stem in conv_filenames:
+        #     continue
+
+        # forbidden characters: =, +, :
+        characters = ["=", "+", ":"]
+        for ch in characters:
+            correct_filename = f.name.replace(ch, "")
+        if correct_filename != f.name:
+            correct_path = f.parent / correct_filename
+            f = f.rename(correct_path)
+
+            corrected_num += 1
+
+    return corrected_num
 
 
-def prepare_imgs():
-    raw_dir = os.path.join(DATA_FOLDER, 'raw')
-    prep_dir = os.path.join(DATA_FOLDER, 'prepared')
+def resize_dir_imgs(raw_dir, prep_dir, conv_filenames):
+    new_conv_num = 0
+    new_filenames = []
+    for f in raw_dir.glob("*"):
+        if f.stem in conv_filenames:
+            continue
 
-    if not os.path.exists(prep_dir):
-        os.mkdir(prep_dir)
+        # TODO: rename intial file
+        # forbidden characters: =, +, :
+        characters = ["=", "+", ":"]
+        for ch in characters:
+            correct_filename = f.name.replace(ch, "")
+        if correct_filename != f.name:
+            correct_path = f.parent / correct_filename
+            f = f.rename(correct_path)
 
-    for dn in os.listdir(raw_dir):
-        dpath = os.path.join(raw_dir, dn)
-        prep_subdir = os.path.join(prep_dir, dn)
-        if not os.path.exists(prep_subdir):
-            os.mkdir(prep_subdir)
-        for fn in os.listdir(dpath):
-            fpath = os.path.join(dpath, fn)
-            if not os.path.isfile(fpath):
-                continue
-            img = cv2.imread(fpath, cv2.IMREAD_UNCHANGED)
-            changed = resize_img(img)
-            prep_fpath = os.path.join(prep_subdir, fn)
-            cv2.imwrite(prep_fpath, changed)
+        img = cv2.imread(str(f), cv2.IMREAD_UNCHANGED)
+        changed = resize_img(img)
+        cv2.imwrite(str(prep_dir / (f.stem + ".png")), changed)
+
+        new_conv_num += 1
+        new_filenames.append(f.stem)
+    return new_conv_num, new_filenames
 
 
-prepare_imgs()
+# def prepare_imgs(glob_raw_dir, glob_prep_dir, species):
+#     glob_raw_dir = Path(glob_raw_dir)
+#     glob_prep_dir = Path(glob_prep_dir)
+
+#     glob_prep_dir.mkdir(exist_ok=True)
+
+#     for s in species:
+#         s_raw_dir = glob_raw_dir / s
+#         s_prep_dir = glob_prep_dir / s
+
+#         for t in ['downside', 'exist']:
+#             prep_dir = s_prep_dir / t
+
+#             prep_dir.mkdir(exist_ok=True, parents=True)
+#             resize_dir_imgs(s_raw_dir / t, prep_dir)
+
+
+def prepare_imgs(glob_raw_dir, glob_prep_dir, metadata_path):
+    glob_raw_dir = Path(glob_raw_dir)
+    glob_prep_dir = Path(glob_prep_dir)
+    metadata_path = Path(metadata_path)
+
+    if metadata_path.exists():
+        metadata = pd.read_csv(metadata_path)
+    else:
+        metadata_path.parent.mkdir(parents=True, exist_ok=True)
+        metadata = pd.DataFrame(columns=["filename"])
+    conv_filenames = set(metadata["filename"])
+
+    glob_prep_dir.mkdir(exist_ok=True)
+
+    correct_filenames(glob_raw_dir, conv_filenames)
+
+    tot_conv_num = 0
+    conv_imgs_limit = 90000
+    tot_new_filenames = []
+    for raw_dir in tqdm(sorted(list(glob_raw_dir.glob("*")))):
+        if tot_conv_num > conv_imgs_limit:
+            break
+
+        prep_dir = glob_prep_dir  # / raw_dir.name
+
+        prep_dir.mkdir(exist_ok=True)
+        new_conv_num, new_filenames = resize_dir_imgs(raw_dir, prep_dir, conv_filenames)
+
+        tot_conv_num += new_conv_num
+        tot_new_filenames = tot_new_filenames + new_filenames
+
+    new_metadata = pd.DataFrame({"filename": tot_new_filenames})
+    metadata = pd.concat([metadata, new_metadata]).reset_index(drop=True)
+
+    metadata.to_csv(metadata_path, index=False)
+
+    print(f"Converted images: {tot_conv_num}")
+
+
+def create_unlabeled_split_csv(data_dir, split_path):
+    data_dir = Path(data_dir)
+    split_path = Path(split_path)
+
+    paths = []
+    for f_path in sorted(list(data_dir.glob("*"))):
+        f_path = str(Path(f_path.parent.name) / f_path.name)
+        paths.append(f_path)
+    metadata = pd.DataFrame({"path": pd.Series(paths)})
+
+    print(metadata.head())
+    print(metadata.shape[0])
+
+    metadata.to_csv(split_path, index=False)
+
+
+glob_raw_dir = "data/fungi_train_val/images"
+glob_prep_dir = "data/unlabeled/challenge2018/images"
+conv_path = "data/metadata/challenge2018.csv"
+split_path = "data/unlabeled/challenge2018/split.csv"
+# species = ['agaricus_xanthodermus', 'amanita_muscaria', 'amanita_phalloides']
+# prepare_imgs(glob_raw_dir, glob_prep_dir, conv_path)  # , species)
+create_unlabeled_split_csv(glob_prep_dir, split_path)
+# corrected_num = correct_filenames(glob_prep_dir, None)
+# print(corrected_num)
--- data_scripts/scrap_script.py
+++ data_scripts/scrap_script.py
@@ -7,15 +7,18 @@ import pandas as pd
 import os.path
 import time
 from tqdm import tqdm
+from datetime import datetime
 
 DATA_FOLDER = 'data/'
+LOG_FOLDER = 'data_scripts/logs/'
+
 
 def load_data():
     taxon_paths = {
         # "armillaria_mellea": "66576, 60212, 60059, 66090, 66108, 60012, 60011, 66244, 50126, 10526",
         # "boletus_edulis": "66576, 60212, 60059, 66090, 66108, 60012, 60066, 60065, 50226, 11069",
         # "suillus_luteus": "66576, 60212, 60059, 66090, 66108, 60012, 60066, 60556, 51902, 20734",
-        # "cantharellus_cibarius": "66576, 60212, 60059, 66090, 66108, 60083, 60082, 50309, 11317"
+        # "cantharellus_cibarius": "66576, 60212, 60059, 66090, 66108, 60083, 60082, 50309, 11317",
         # "russula_vesca": "66576, 60212, 60059, 66090, 66108, 60509, 60508, 51729, 20093",
         # "amanita_phalloides": "66576, 60212, 60059, 66090, 66108, 60012, 60011, 66237, 50051, 63478",
         # "rubroboletus_satanas": "66576, 60212, 60059, 66090, 66108, 60012, 60066, 60065, 65782, 65783",
@@ -34,49 +37,62 @@ def load_data():
     visited_df = open_visited_df(visited_path)
     new_df = pd.DataFrame(columns=['id', 'species'])
 
-    for species, taxon_path in taxon_paths.items():
-        print(f'Species {species} started')
-        root_url, query_str = parse_url(taxon_path, limit, offset)
+    try:
+        for species, taxon_path in taxon_paths.items():
+            print(f'Species {species} started')
+            root_url, query_str = parse_url(taxon_path, limit, offset)
 
-        resp = requests.get(root_url, params=query_str)
-        data = resp.json()
+            resp = requests.get(root_url, params=query_str)
+            data = resp.json()
 
-        create_species_folder(species)
+            create_species_folder(species)
 
-        cur_visited = set(visited_df[visited_df['species'] == species]['id'])
-        cur_new = []
-        for record in tqdm(data):
-            time.sleep(0.01)
+            cur_visited = set(
+                visited_df[visited_df['species'] == species]['id'])
+            cur_new = []
+            try:
+                for record in tqdm(data):
+                    time.sleep(0.01)
 
-            if record["_id"] in cur_visited:
-                continue
-            else:
-                cur_new.append(record["_id"])
+                    record_id = record["_id"]
+                    if record_id in cur_visited:
+                        continue
+                    else:
+                        cur_new.append(record_id)
 
-            if "Images" not in record:
-                continue
+                    if "Images" not in record:
+                        continue
 
-            for img in record["Images"]:
-                img_id = img["_id"]
-                img_name = img["name"]
+                    for img in record["Images"]:
+                        img_id = img["_id"]
+                        img_name = img["name"]
 
-                # start = time.time()
-                img_url = """https://svampe.databasen.org/uploads/""" + img_name + """.JPG"""
-                img = Image.open(requests.get(img_url, stream=True).raw)
-                # stop = time.time()
-                # print(f'img: {img_name}, load time: {stop-start}')
-                
-                # start = time.time()
-                img_path = os.path.join(DATA_FOLDER, 'raw', species, f'{img_name}.png')
-                img.save(img_path)
-                # stop = time.time()
-                # print(f'img: {img_name}, save time: {stop-start}')
+                        # start = time.time()
+                        img_url = """https://svampe.databasen.org/uploads/""" + img_name + """.JPG"""
+                        img = Image.open(requests.get(img_url, stream=True).raw)
+                        # stop = time.time()
+                        # print(f'img: {img_name}, load time: {stop-start}')
 
-        cur_new_df = pd.DataFrame(cur_new, columns=['id'])
-        cur_new_df['species'] = species
-        new_df = pd.concat([new_df, cur_new_df])
-    
-    save_visited_df(visited_path, visited_df, new_df)
+                        # start = time.time()
+                        img_path = os.path.join(
+                            DATA_FOLDER, 'raw', species, f'{img_name}.png')
+                        img.save(img_path)
+                        # stop = time.time()
+                        # print(f'img: {img_name}, save time: {stop-start}')
+            except:
+                if not os.path.exists(LOG_FOLDER):
+                    os.mkdir(LOG_FOLDER)
+                datetime_now = datetime.now().strftime("%d-%m-%Y_%H-%M-%S")
+                log_path = os.path.join(LOG_FOLDER, f'log_{datetime_now}')
+                with open(log_path, 'a+') as f:
+                    f.write(f'FAILED record id: {record_id}, image url: {img_url}\n')
+            finally:
+                cur_new_df = pd.DataFrame(cur_new, columns=['id'])
+                cur_new_df['species'] = species
+                new_df = pd.concat([new_df, cur_new_df])
+    finally:
+        save_visited_df(visited_path, visited_df, new_df)
+
 
 def open_visited_df(visited_path):
     if os.path.exists(visited_path):
@@ -85,14 +101,17 @@ def open_visited_df(visited_path):
         visited_df = pd.DataFrame(columns=['id', 'species'])
     return visited_df
 
+
 def save_visited_df(visited_path, visited_df, new_df):
     visited_df = pd.concat([visited_df, new_df])
     visited_df.to_csv(visited_path, index=False)
+
 
 def create_species_folder(species):
     species_path = os.path.join(DATA_FOLDER, 'raw', species)
     if not os.path.exists(species_path):
         os.mkdir(species_path)
+
 
 def parse_url(taxon_path, limit, offset):
     initial_url = """https://svampe.databasen.org/api/observations?_order=%5B%5B%22observationDate%22,%22DESC%22,%22ASC%22%5D,%5B%22_id%22,%22DESC%22%5D%5D&include=%5B%22%7B%5C%22model%5C%22:%5C%22DeterminationView%5C%22,%5C%22as%5C%22:%5C%22DeterminationView%5C%22,%5C%22attributes%5C%22:%5B%5C%22Taxon_id%5C%22,%5C%22Recorded_as_id%5C%22,%5C%22Taxon_FullName%5C%22,%5C%22Taxon_vernacularname_dk%5C%22,%5C%22Taxon_RankID%5C%22,%5C%22Determination_validation%5C%22,%5C%22Taxon_redlist_status%5C%22,%5C%22Taxon_path%5C%22,%5C%22Recorded_as_FullName%5C%22,%5C%22Determination_user_id%5C%22,%5C%22Determination_score%5C%22,%5C%22Determination_validator_id%5C%22,%5C%22Determination_species_hypothesis%5C%22,%5C%22Determination_notes%5C%22%5D,%5C%22where%5C%22:%7B%5C%22$and%5C%22:%7B%5C%22$or%5C%22:%5B%5D%7D,%5C%22$or%5C%22:%5B%7B%5C%22Taxon_path%5C%22:%7B%5C%22like%5C%22:%5C%2266576,+60212,+60059,+66090,+66108,+60012,+60066,+60065,+50226,+11069%25%5C%22%7D%7D%5D%7D%7D%22,%22%7B%5C%22model%5C%22:%5C%22User%5C%22,%5C%22as%5C%22:%5C%22PrimaryUser%5C%22,%5C%22required%5C%22:false,%5C%22where%5C%22:%7B%7D%7D%22,%22%7B%5C%22model%5C%22:%5C%22Locality%5C%22,%5C%22as%5C%22:%5C%22Locality%5C%22,%5C%22attributes%5C%22:%5B%5C%22_id%5C%22,%5C%22name%5C%22%5D,%5C%22where%5C%22:%7B%7D,%5C%22required%5C%22:true%7D%22,%22%7B%5C%22model%5C%22:%5C%22GeoNames%5C%22,%5C%22as%5C%22:%5C%22GeoNames%5C%22,%5C%22where%5C%22:%7B%7D,%5C%22required%5C%22:false%7D%22,%22%7B%5C%22model%5C%22:%5C%22ObservationUser%5C%22,%5C%22as%5C%22:%5C%22userIds%5C%22,%5C%22where%5C%22:%7B%7D,%5C%22required%5C%22:false%7D%22,%22%7B%5C%22model%5C%22:%5C%22ObservationImage%5C%22,%5C%22as%5C%22:%5C%22Images%5C%22,%5C%22where%5C%22:%7B%7D,%5C%22required%5C%22:false%7D%22,%22%7B%5C%22model%5C%22:%5C%22ObservationForum%5C%22,%5C%22as%5C%22:%5C%22Forum%5C%22,%5C%22where%5C%22:%7B%7D,%5C%22required%5C%22:false%7D%22,%22%7B%5C%22model%5C%22:%5C%22ObservationArea%5C%22,%5C%22as%5C%22:%5C%22areaIds%5C%22,%5C%22where%5C%22:%7B%7D,%5C%22required%5C%22:false%7D%22,%22%7B%5C%22model%5C%22:%5C%22Determination%5C%22,%5C%22as%5C%22:%5C%22Determinations%5C%22,%5C%22where%5C%22:%7B%7D,%5C%22attributes%5C%22:%5B%5C%22_id%5C%22,%5C%22score%5C%22%5D,%5C%22required%5C%22:false%7D%22,%22%7B%5C%22model%5C%22:%5C%22DnaSequence%5C%22,%5C%22as%5C%22:%5C%22DnaSequence%5C%22,%5C%22where%5C%22:%7B%7D,%5C%22attributes%5C%22:%5B%5C%22_id%5C%22,%5C%22marker%5C%22,%5C%22geneticAccessionNumber%5C%22%5D,%5C%22required%5C%22:false%7D%22%5D&limit=5&offset=0&where=%7B%7D"""
@@ -102,8 +121,8 @@ def parse_url(taxon_path, limit, offset):
     root_url = parsed._replace(query=None).geturl()
 
     taxon_path = quote_plus(taxon_path)
-    include_param = """%5B%22%7B%5C%22model%5C%22:%5C%22DeterminationView%5C%22,%5C%22as%5C%22:%5C%22DeterminationView%5C%22,%5C%22attributes%5C%22:%5B%5C%22Taxon_id%5C%22,%5C%22Recorded_as_id%5C%22,%5C%22Taxon_FullName%5C%22,%5C%22Taxon_vernacularname_dk%5C%22,%5C%22Taxon_RankID%5C%22,%5C%22Determination_validation%5C%22,%5C%22Taxon_redlist_status%5C%22,%5C%22Taxon_path%5C%22,%5C%22Recorded_as_FullName%5C%22,%5C%22Determination_user_id%5C%22,%5C%22Determination_score%5C%22,%5C%22Determination_validator_id%5C%22,%5C%22Determination_species_hypothesis%5C%22,%5C%22Determination_notes%5C%22%5D,%5C%22where%5C%22:%7B%5C%22$and%5C%22:%7B%5C%22$or%5C%22:%5B%5D%7D,%5C%22$or%5C%22:%5B%7B%5C%22Taxon_path%5C%22:%7B%5C%22like%5C%22:%5C%22""" + taxon_path + """%25%5C%22%7D%7D%5D%7D%7D%22,%22%7B%5C%22model%5C%22:%5C%22User%5C%22,%5C%22as%5C%22:%5C%22PrimaryUser%5C%22,%5C%22required%5C%22:false,%5C%22where%5C%22:%7B%7D%7D%22,%22%7B%5C%22model%5C%22:%5C%22Locality%5C%22,%5C%22as%5C%22:%5C%22Locality%5C%22,%5C%22attributes%5C%22:%5B%5C%22_id%5C%22,%5C%22name%5C%22%5D,%5C%22where%5C%22:%7B%7D,%5C%22required%5C%22:true%7D%22,%22%7B%5C%22model%5C%22:%5C%22GeoNames%5C%22,%5C%22as%5C%22:%5C%22GeoNames%5C%22,%5C%22where%5C%22:%7B%7D,%5C%22required%5C%22:false%7D%22,%22%7B%5C%22model%5C%22:%5C%22ObservationUser%5C%22,%5C%22as%5C%22:%5C%22userIds%5C%22,%5C%22where%5C%22:%7B%7D,%5C%22required%5C%22:false%7D%22,%22%7B%5C%22model%5C%22:%5C%22ObservationImage%5C%22,%5C%22as%5C%22:%5C%22Images%5C%22,%5C%22where%5C%22:%7B%7D,%5C%22required%5C%22:false%7D%22,%22%7B%5C%22model%5C%22:%5C%22ObservationForum%5C%22,%5C%22as%5C%22:%5C%22Forum%5C%22,%5C%22where%5C%22:%7B%7D,%5C%22required%5C%22:false%7D%22,%22%7B%5C%22model%5C%22:%5C%22ObservationArea%5C%22,%5C%22as%5C%22:%5C%22areaIds%5C%22,%5C%22where%5C%22:%7B%7D,%5C%22required%5C%22:false%7D%22,%22%7B%5C%22model%5C%22:%5C%22Determination%5C%22,%5C%22as%5C%22:%5C%22Determinations%5C%22,%5C%22where%5C%22:%7B%7D,%5C%22attributes%5C%22:%5B%5C%22_id%5C%22,%5C%22score%5C%22%5D,%5C%22required%5C%22:false%7D%22,%22%7B%5C%22model%5C%22:%5C%22DnaSequence%5C%22,%5C%22as%5C%22:%5C%22DnaSequence%5C%22,%5C%22where%5C%22:%7B%7D,%5C%22attributes%5C%22:%5B%5C%22_id%5C%22,%5C%22marker%5C%22,%5C%22geneticAccessionNumber%5C%22%5D,%5C%22required%5C%22:false%7D%22%5D"""
-    
+    include_param = """%5B%22%7B%5C%22model%5C%22:%5C%22DeterminationView%5C%22,%5C%22as%5C%22:%5C%22DeterminationView%5C%22,%5C%22attributes%5C%22:%5B%5C%22Taxon_id%5C%22,%5C%22Recorded_as_id%5C%22,%5C%22Taxon_FullName%5C%22,%5C%22Taxon_vernacularname_dk%5C%22,%5C%22Taxon_RankID%5C%22,%5C%22Determination_validation%5C%22,%5C%22Taxon_redlist_status%5C%22,%5C%22Taxon_path%5C%22,%5C%22Recorded_as_FullName%5C%22,%5C%22Determination_user_id%5C%22,%5C%22Determination_score%5C%22,%5C%22Determination_validator_id%5C%22,%5C%22Determination_species_hypothesis%5C%22,%5C%22Determination_notes%5C%22%5D,%5C%22where%5C%22:%7B%5C%22$and%5C%22:%7B%5C%22$or%5C%22:%5B%5D%7D,%5C%22$or%5C%22:%5B%7B%5C%22Taxon_path%5C%22:%7B%5C%22like%5C%22:%5C%22""" + taxon_path + \
+        """%25%5C%22%7D%7D%5D%7D%7D%22,%22%7B%5C%22model%5C%22:%5C%22User%5C%22,%5C%22as%5C%22:%5C%22PrimaryUser%5C%22,%5C%22required%5C%22:false,%5C%22where%5C%22:%7B%7D%7D%22,%22%7B%5C%22model%5C%22:%5C%22Locality%5C%22,%5C%22as%5C%22:%5C%22Locality%5C%22,%5C%22attributes%5C%22:%5B%5C%22_id%5C%22,%5C%22name%5C%22%5D,%5C%22where%5C%22:%7B%7D,%5C%22required%5C%22:true%7D%22,%22%7B%5C%22model%5C%22:%5C%22GeoNames%5C%22,%5C%22as%5C%22:%5C%22GeoNames%5C%22,%5C%22where%5C%22:%7B%7D,%5C%22required%5C%22:false%7D%22,%22%7B%5C%22model%5C%22:%5C%22ObservationUser%5C%22,%5C%22as%5C%22:%5C%22userIds%5C%22,%5C%22where%5C%22:%7B%7D,%5C%22required%5C%22:false%7D%22,%22%7B%5C%22model%5C%22:%5C%22ObservationImage%5C%22,%5C%22as%5C%22:%5C%22Images%5C%22,%5C%22where%5C%22:%7B%7D,%5C%22required%5C%22:false%7D%22,%22%7B%5C%22model%5C%22:%5C%22ObservationForum%5C%22,%5C%22as%5C%22:%5C%22Forum%5C%22,%5C%22where%5C%22:%7B%7D,%5C%22required%5C%22:false%7D%22,%22%7B%5C%22model%5C%22:%5C%22ObservationArea%5C%22,%5C%22as%5C%22:%5C%22areaIds%5C%22,%5C%22where%5C%22:%7B%7D,%5C%22required%5C%22:false%7D%22,%22%7B%5C%22model%5C%22:%5C%22Determination%5C%22,%5C%22as%5C%22:%5C%22Determinations%5C%22,%5C%22where%5C%22:%7B%7D,%5C%22attributes%5C%22:%5B%5C%22_id%5C%22,%5C%22score%5C%22%5D,%5C%22required%5C%22:false%7D%22,%22%7B%5C%22model%5C%22:%5C%22DnaSequence%5C%22,%5C%22as%5C%22:%5C%22DnaSequence%5C%22,%5C%22where%5C%22:%7B%7D,%5C%22attributes%5C%22:%5B%5C%22_id%5C%22,%5C%22marker%5C%22,%5C%22geneticAccessionNumber%5C%22%5D,%5C%22required%5C%22:false%7D%22%5D"""
 
     query['include'] = [include_param]
     query['limit'] = [str(limit)]
@@ -116,9 +135,10 @@ def parse_url(taxon_path, limit, offset):
 
     return root_url, query_str
 
+
 if __name__ == "__main__":
     load_data()
 
-# 1. don't resize before saving - may want to apply another algorithm
-# 2. doesn't worth to parallel saving - 1 bus is used
-# 3. rewrite to save if exception
+# 1. don't resize before saving - may want to apply another algorithm +
+# 2. doesn't worth to parallel saving - 1 bus is used +
+# 3. rewrite to save if exception +
